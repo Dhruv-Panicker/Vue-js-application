e39eca1f6ff564046398348d8f1995be19bc0480
Dhruv Panicker <dhru.panicker@gmail.com>
2023-05-07 21:15:55 -0400

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,12 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<router-view />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
 }
 </script>
 
@@ -23,4 +19,27 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+
+.register input, .login input {
+    width: 300px; 
+    height: 40px; 
+    padding-left: 20px; 
+    display: block; 
+    margin-bottom: 30px;
+    margin-right: auto; 
+    margin-left: auto; 
+    border: 1px solid skyblue; 
+
+}
+
+.register button, .login button  {
+    width: 320px; 
+    height: 40px;
+    border: 1px solid skyblue;
+    background: skyblue; 
+    color: white; 
+    cursor: pointer;
+}
+
 </style>
---
+++ src/components/HomePage.vue
@@ -0,0 +1,20 @@
+<template>
+    <h1>Hello User, This is the homepage!</h1>
+</template>
+
+
+<script>
+
+export default {
+    name:'HomePage', 
+    mounted()
+    {
+        let user= localStorage.getItem('user-info'); 
+        if(!user){
+            this.$router.push({name:'SignUp'})
+        }
+    }
+}
+</script>
+
+
